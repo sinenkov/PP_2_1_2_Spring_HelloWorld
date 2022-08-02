ea8625b3e007f9b456fe0bb503980bf55bc223a7
sinenkov <107352549+sinenkov@users.noreply.github.com>
2022-08-02 15:00:00 +0500

    start

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,11 +3,12 @@ public class HelloWorld {
     private String message;
  
     public String getMessage() {
+        System.out.println();
         return message;
     }
  
     public void setMessage(String message) {
         this.message = message;
     }
-     
+
 }
